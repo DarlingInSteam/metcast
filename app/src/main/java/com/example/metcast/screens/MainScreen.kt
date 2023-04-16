f40f156501e2b2876c06e659b95de2a21a5dcb30
darlingPc <rombk2711@gmail.com>
2023-04-16 23:39:09 +0700

    refactoring

--- app/src/main/java/com/example/metcast/screens/MainScreen.kt
+++ app/src/main/java/com/example/metcast/screens/MainScreen.kt
@@ -1,8 +1,6 @@
 package com.example.metcast.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -14,7 +12,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
