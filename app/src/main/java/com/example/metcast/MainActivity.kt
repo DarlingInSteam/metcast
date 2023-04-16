1bcd7178c5b8a9116481af78d4fb6b424e1ddb88
darlingPc <rombk2711@gmail.com>
2023-04-16 19:29:48 +0700

    add coil impl

--- app/src/main/java/com/example/metcast/MainActivity.kt
+++ app/src/main/java/com/example/metcast/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.metcast.screens.MainScreen
 import com.example.metcast.ui.theme.MetcastTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MetcastTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MetcastTheme {
-        Greeting("Android")
     }
 }
