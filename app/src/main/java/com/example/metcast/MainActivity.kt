9159e0eda4b9c0492953e43e3f422c9a5979b51e
darlingPc <rombk2711@gmail.com>
2023-04-16 22:19:52 +0700

    fix request from weather api

--- app/src/main/java/com/example/metcast/MainActivity.kt
+++ app/src/main/java/com/example/metcast/MainActivity.kt
@@ -1,24 +1,40 @@
 package com.example.metcast
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.metcast.data.WeatherModule
 import com.example.metcast.screens.MainCard
 import com.example.metcast.screens.TabLayout
 import com.example.metcast.ui.theme.MetcastTheme
+import org.json.JSONObject
+
+const val API_KEY = "b57591f88e1640c188b123207231604"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MetcastTheme {
+                val daysList = remember {
+                    mutableStateOf(listOf<WeatherModule>())
+                }
+                GetData("London", this, daysList)
                 Image(
                     painter = painterResource(id = R.drawable.background),
                     contentDescription = "im1",
@@ -29,9 +45,72 @@ class MainActivity : ComponentActivity() {
                 )
                 Column {
                     MainCard()
-                    TabLayout()
+                    TabLayout(daysList)
                 }
             }
         }
     }
+}
+
+private fun GetData(city: String, context: Context, daysList: MutableState<List<WeatherModule>>) {
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
+            "&q=$city" +
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no"
+
+    val queue = Volley.newRequestQueue(context)
+
+    val request = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+            responseBody ->
+            val list = GetWeatherByDays(responseBody)
+            daysList.value = list
+        },
+        {
+            Log.d("Error", "Error in request: $it" )
+        },
+    )
+
+    queue.add(request)
+}
+
+private fun GetWeatherByDays(response: String): List<WeatherModule> {
+    if (response.isEmpty()) return listOf()
+
+    val list = ArrayList<WeatherModule>()
+    val mainObj = JSONObject(response)
+
+    val city = mainObj.getJSONObject("location").getString("name")
+    val days = mainObj.getJSONObject("forecast").getJSONArray("forecastday")
+
+    for (i in 0 until days.length()) {
+        val item = days[i] as JSONObject
+
+        list.add(
+            WeatherModule(
+                city,
+                item.getString("date"),
+                "",
+                item.getJSONObject("day")
+                    .getJSONObject("condition")
+                    .getString("text"),
+                item.getJSONObject("day")
+                    .getJSONObject("condition")
+                    .getString("icon"),
+                item.getJSONObject("day").getString("maxtemp_c"),
+                item.getJSONObject("day").getString("mintemp_c"),
+                item.getJSONArray("hour").toString(),
+            )
+        )
+    }
+
+    list[0] = list[0].copy(
+        time = mainObj.getJSONObject("current").getString("last_updated"),
+        tempCurrent = mainObj.getJSONObject("current").getString("temp_c")
+    )
+
+    return list
 }
--- app/src/main/java/com/example/metcast/screens/MainScreen.kt
+++ app/src/main/java/com/example/metcast/screens/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -113,7 +114,7 @@ fun MainCard() {
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-fun TabLayout() {
+fun TabLayout(daysList: MutableState<List<WeatherModule>>) {
     val tabList = listOf("HOURS", "DAYS")
     val pagerState = rememberPagerState()
     val tabIndex = pagerState.currentPage
@@ -159,28 +160,7 @@ fun TabLayout() {
                 modifier = Modifier.fillMaxSize()
             ) {
                 itemsIndexed(
-                    listOf(
-                        WeatherModule(
-                            "London",
-                            "10:00",
-                            "16°C",
-                            "Sunny",
-                            "//cdn.weatherapi.com/weather/64x64/day/116.png",
-                            "",
-                            "",
-                            "",
-                        ),
-                        WeatherModule(
-                            "London",
-                            "26/07/2020",
-                            "",
-                            "Sunny",
-                            "//cdn.weatherapi.com/weather/64x64/day/116.png",
-                            "26°",
-                            "12°",
-                            "not empty",
-                        )
-                    )
+                    daysList.value
                 ) { _, item ->
                     ListItem(item)
                 }
